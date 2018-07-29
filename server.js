fc623512a939d68bbf91ad0277e23bbd57e01919
Nicole Cloyd <Nicole@nicole-cloyds-macbook-pro.local>
2018-07-29 17:40:25 -0500

    linked css sheet

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'app/public')));
-//app.use(express.static('app/public'));
+app.use(express.static(path.join(__dirname, './app/public')));
+//app.use(express.static("/public"));
+//app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
