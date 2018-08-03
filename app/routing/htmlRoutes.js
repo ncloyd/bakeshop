f14a7d1e41d2835d75c5d48a677118742e0ccc2f
Nicole Cloyd <Nicole@nicole-cloyds-macbook-pro.local>
2018-08-02 21:25:59 -0500

    added pages

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,8 +2,17 @@ var path = require("path");
 
 module.exports = function(app) {
 //GET requests
-	app.get("/creations", function(req, res) {
-		res.sendFile(path.join(__dirname, "/../public/creations.html"));
+	//cupcakes page
+	app.get("/cupcakes", function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/cupcakes.html"));
+	});
+	//cakes page
+	app.get("/cakes", function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/cakes.html"));
+	});
+	//cookies page
+	app.get("/cookies", function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/cookies.html"));
 	});
 
 //default to home if no route exists
